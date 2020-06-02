54604a51923127f70ce30eec1b6835175a81fc59
Lucas Frank <lucasfrank_@hotmail.com>
2020-06-02 20:52:03 -0300

    Implementados testes para deques.

--- StackAndQueueTest.cpp
+++ StackAndQueueTest.cpp
@@ -1,6 +1,7 @@
 //Autor: Lucas Frank Hollmann
 
 #include "Fila.h"
+#include "Deque.h"
 
 #include <stdio.h> //Biblioteca para usar printf.
 
@@ -8,7 +9,9 @@ int main()
 {
 	
 	Queue Q;
+	Deque DQ;
 	
+	printf("Queue:\n");
 	Q.isEmpty();
 	Q.push(1);
 	Q.push(2);
@@ -20,6 +23,47 @@ int main()
 	Q.printQueue();
 	Q.getSize();
 	Q.isEmpty();
+	
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	printf("pop %d\n", *Q.pop());Q.getSize();
+	int *d = Q.pop();
+	if(d !=NULL)
+	{
+		printf("pop %d\n", *d);Q.getSize();
+	}
+	
+	printf("Deque:\n");
+	DQ.isEmpty();
+	DQ.pushRight(1);
+	DQ.pushLeft(2);
+	DQ.pushRight(3);
+	DQ.pushLeft(4);
+	DQ.pushRight(5);
+	DQ.pushLeft(6);
+	DQ.pushRight(7);
+	DQ.printQueue();
+	DQ.getSize();
+	DQ.isEmpty();
+	
+	printf("popL %d\n", *DQ.popLeft());DQ.printQueue();DQ.getSize();
+	printf("popR %d\n", *DQ.popRight());DQ.printQueue();DQ.getSize();
+	printf("popL %d\n", *DQ.popLeft());DQ.printQueue();DQ.getSize();
+	printf("popR %d\n", *DQ.popRight());DQ.printQueue();DQ.getSize();
+	printf("popL %d\n", *DQ.popLeft());DQ.printQueue();DQ.getSize();
+	printf("popR %d\n", *DQ.popRight());DQ.printQueue();DQ.getSize();
+	printf("popL %d\n", *DQ.popLeft());DQ.printQueue();DQ.getSize();
+	
+	d = DQ.popRight();
+	if(d !=NULL)
+	{
+		printf("popR %d\n", *d);DQ.printQueue();DQ.getSize();
+	}
+	
 	
 	
 	return 0;
